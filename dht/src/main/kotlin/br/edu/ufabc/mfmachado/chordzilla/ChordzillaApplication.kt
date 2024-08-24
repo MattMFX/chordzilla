b507ea13f095ee3c3bb6a5e4f2eb71aaa33c52ad
MattMFX <matheus0210@hotmail.com>
2024-08-24 10:00:15 -0300

    project structure

---
+++ dht/src/main/kotlin/br/edu/ufabc/mfmachado/chordzilla/ChordzillaApplication.kt
@@ -0,0 +1,11 @@
+package br.edu.ufabc.mfmachado.chordzilla
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChordzillaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChordzillaApplication>(*args)
+}
