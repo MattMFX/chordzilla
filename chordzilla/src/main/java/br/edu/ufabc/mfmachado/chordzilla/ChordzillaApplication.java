fc9b40c38a8bcec9f0bbdd32c21c67653175182e
MattMFX <matheus0210@hotmail.com>
2024-08-26 23:58:21 -0300

    initial protobuf file

---
+++ chordzilla/src/main/java/br/edu/ufabc/mfmachado/chordzilla/ChordzillaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufabc.mfmachado.chordzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChordzillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChordzillaApplication.class, args);
+	}
+
+}
---
+++ chordzilla/src/test/java/br/edu/ufabc/mfmachado/chordzilla/ChordzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufabc.mfmachado.chordzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChordzillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
